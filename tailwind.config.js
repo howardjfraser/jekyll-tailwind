c1b34f12c6b995ba6133c11f332112c580d5f671
Howard Fraser <howardjfraser@gmail.com>
2019-08-26 13:28:42 +1000

    Tinker.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   theme: {
+    screens: {
+      sm: "480px",
+      md: "640px",
+      lg: "768px",
+      xl: "1024px"
+    },
     extend: {
       fontFamily: {
         sans: ["adelle-sans", ...defaultTheme.fontFamily.sans]
