25dd83d620848399a906328bffc1d2b12e36dad6
Howard Fraser <howardjfraser@gmail.com>
2019-10-02 20:22:28 +1000

    Break points.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   theme: {
     extend: {
+      screens: {
+        sm: "480px",
+        md: "640px",
+        lg: "768px",
+        xl: "1024px"
+      },
       fontFamily: {
         sans: ["adelle-sans", ...defaultTheme.fontFamily.sans]
       }
